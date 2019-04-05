6955b5beccb8252ea1053ab1f2600caf31db2ec7
Elturco23 <jrianoa@uninorte.edu.co>
2019-04-05 22:45:34 +0700

    crear metodo fw

--- src/floydwarshall/Floydwarshall.java
+++ src/floydwarshall/Floydwarshall.java
@@ -5,17 +5,41 @@
  */
 package floydwarshall;
 
-/**
- *
- * @author JORGE RIAÑO
- */
+
 public class Floydwarshall {
 
-    /**
-     * @param args the command line arguments
-     */
+    static void Floy(int grafo[][]){
+    int n= grafo.length, tmp;
+    //crear matriz de distancia
+    int distancias[][]= new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                distancias[i][j]=j;
+            }
+        }
+        //ejecutar el metodo floy
+        for (int k = 0; k < 10; k++) {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    tmp= grafo[i][k]+grafo[k][j];
+                    if(tmp<grafo[i][j]){
+                    grafo[i][j]=tmp;
+                    distancias[i][j]=k;
+                    }
+                }
+            }
+        }
+    
+    }
+    static void print (int grafo[][]){
+        for (int i = 0; i < grafo.length; i++) {
+            for (int j = 0; j < grafo.length; j++) {
+                System.out.print(grafo[i][j]+"  ");
+            }
+        }
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+    
     }
     
 }
